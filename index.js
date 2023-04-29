f5624e68d7a7ec6aba9dd6574e513582c6ba72ed
alperaktepee <alperaktepe98@gmail.com>
2023-04-29 18:02:53 +0300

    played sounds

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var numberOfDrumButtons=document.querySelectorAll(".drum").length;
-for (var i = 0; i <numberOfDrumButtons ; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick)
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+      this.style.color = "white";  
+  });
 }
 
 
-  function handleClick() {
-    alert("I got clicked");
-  }
+var audio = new Audio('sounds/tom-1.mp3');
+audio.play();
